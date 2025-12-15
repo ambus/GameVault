5833f07cd70d9193bf5de25139bcddf8c85567cc
Szymon Standarski <ambus@o2.pl>
2025-12-15 16:58:25 +0100

    init project

---
+++ src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
+  ]
+};
