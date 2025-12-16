1401219cc7b5ea2fb2a5a98e772670cffde4e9d2
Szymon Standarski <ambus@o2.pl>
2025-12-16 15:33:34 +0100

    feat: add game button component with styling for game addition

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 import { HeaderComponent } from '../ui/header/header.component';
 
 @Component({
   standalone: true,
   selector: 'app-layout',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, ButtonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
